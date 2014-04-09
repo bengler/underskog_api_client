3efc32389d56d8c6f478e72f2c07c196aaa0a016
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2014-04-09 13:03:03 +0200

    Bumb minor version

--- lib/underskog/version.rb
+++ lib/underskog/version.rb
@@ -8,12 +8,12 @@ module Underskog
 
     # @return [Integer]
     def self.minor
-      0
+      1
     end
 
     # @return [Integer]
     def self.patch
-      3
+      0
     end
 
     # @return [String, NilClass]
